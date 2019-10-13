0f954bfa55731e5aacaed95e20a2409b91259372
martezr <martez.reed@greenreedtech.com>
2019-10-13 01:16:35 -0500

    Add create job action

--- actions/create_job.py
+++ actions/create_job.py
@@ -1,14 +1,16 @@
 from lib import action
 import nomad
 
-
 class NomadParseJobAction(action.NomadBaseAction):
     def run(self,file):
         with open(file, "r") as fh:
             try:
                 job_raw_nomad = fh.read()
                 job_dict = self.nomad.jobs.parse(job_raw_nomad)
+                output = {}
+                output['Job'] = job_dict
+                self.nomad.jobs.register_job(output)
             except nomad.api.exceptions.BadRequestNomadException as err:
                 print(err.nomad_resp.reason)
                 print(err.nomad_resp.text)
-        return job_dict
+        return output
